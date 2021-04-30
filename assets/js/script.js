48a3a038e2dbf625b993b8a102673d5cd3fb5974
axelpro04 <axellefpro@gmail.com>
2021-04-30 11:23:49 +0200

    exo

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function touch() {
+    var age = document.getElementById('age').value;
+    if (age >= 18) {
+        alert("Vous êtes majeur");
+    } else {
+        alert("Vous êtes mineur");
+    }
+}
