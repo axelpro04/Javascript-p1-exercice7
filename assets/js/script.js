7cc2eb028ab5334e99d191db2cb5f95c79311b51
axelpro04 <axellefpro@gmail.com>
2021-05-03 10:25:24 +0200

    exo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 function touch() {
     var age = document.getElementById('age').value;
-    if (age >= 18) {
+    if (age >= 18 && age < 110 ) {
         alert("Vous êtes majeur");
-    } else {
+    } else if (age >= 1 && age < 18 ) {
         alert("Vous êtes mineur");
+    } else {
+        alert("Mettez une valeur comprise entre 1 et 110");
     }
+
 }
